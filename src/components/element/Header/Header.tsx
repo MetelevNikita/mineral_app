e829e0e855c7084b45d94f1b8a51cd8d7ababa45
Nikita Metelev <108466638+MetelevNikita@users.noreply.github.com>
2025-09-14 17:17:47 +0500

    create favicon screen 2.1

--- src/components/element/Header/Header.tsx
+++ src/components/element/Header/Header.tsx
@@ -184,7 +184,7 @@ if (!currentUser) {
 
   return (
     
-    <Container className='fixed-top'>
+    <Container className='fixed-top mt-5'>
         <div className={styles.header_container}>
             <div className={styles.header_wrapper}>
 
