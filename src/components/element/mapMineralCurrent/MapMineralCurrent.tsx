df4d17a2269d7a2088fc334abd35e07326a389a9
Nikita Metelev <108466638+MetelevNikita@users.noreply.github.com>
2025-10-09 14:57:40 +0500

    map create forward to mineral page 1.0

--- src/components/element/mapMineralCurrent/MapMineralCurrent.tsx
+++ src/components/element/mapMineralCurrent/MapMineralCurrent.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import Image, { StaticImageData } from 'next/image'
 
 // styles
@@ -9,15 +9,53 @@ import styles from './MapMineralCurrent.module.css'
 
 import IconClose from '@/../public/map/icon_close.svg'
 
+// redux
+
+import { useAppDispatch } from '@/types/hooks'
+import { useAppSelector } from '@/types/hooks'
+
+// async
+
+import { fetchGetAsyncMineral } from '@/functions/reduxAsync/mineral/fetchGetAsyncMineral'
+
+// 
+
 
 interface MapMineralCurrentProps {
+  id: any,
   title: string,
   description: string,
   image: string | StaticImageData
 }
 
 
-const MapMineralCurrent: FC<MapMineralCurrentProps> = ({ title, description, image }) => {
+const MapMineralCurrent: FC<MapMineralCurrentProps> = ({ id, title, description, image }) => {
+
+
+
+  const {currentReduxMneralId, setCurrentReduxMneralId} = id
+
+  console.log(title)
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(
+      fetchGetAsyncMineral()
+    )
+  }, [])
+
+
+  const minerals = useAppSelector(state => state.minerals.minerals)
+  console.log(minerals)
+
+  const currentMineral = minerals.find((mineral) => mineral.title === title)
+
+  if  (currentMineral) {
+      setCurrentReduxMneralId(currentMineral.id)
+  }
+
+
   return (
     <div className={styles.MapMineralCurrent_container}>
 
