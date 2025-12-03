a5ed004e0923e57e4c9ba957d9a4df843de1365c
Nikita Metelev <108466638+MetelevNikita@users.noreply.github.com>
2025-12-03 22:35:18 +0500

    fix prisma migration and create contacts from api

--- src/app/main/contact/page.tsx
+++ src/app/main/contact/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useEffect, useState } from 'react'
 import Link from 'next/link'
 
 // styles
@@ -24,56 +24,62 @@ import map from '@/../public/contacts/map.png'
 
 import { contactsArrType } from '@/types/type'
 
+// fn
+
+import { getContacts } from '@/functions/contacts/getContacts'
+
+
 
 const page: FC = () => {
 
 
-    const contactsArr: contactsArrType[] = [
-        {
-            id: 1,
-            title: 'Адрес',
-            content: '125009, г. Москва, Моховая ул., д. 11, стр. 11 (станция метро «Охотный ряд»)',
-            img: ''
-        },
-
-        {
-            id: 1,
-            title: 'Проведение мероприятий',
-            content: 'Тел.: +7 (495) 692–09–43(будни с 11:00 до 17:00, выходные с 12:00 до 18:00) E-mail: info@sgm.ru',
-            img: ''
-        },
-
-        {
-            id: 1,
-            title: 'Лаборатория',
-            content: 'Тел.: 8 (985) 025-19-71Приём по предварительной записи по телефону. 3 этаж в каб. 338.',
-            img: ''
-        },
-
-        {
-            id: 1,
-            title: 'СМИ',
-            content: 'Лопатина ИринаТел.: +7 (495) 692-09-43E-mail: pr@sgm.ru',
-            img: ''
-        },
-
-        {
-            id: 1,
-            title: 'Как добраться',
-            content: 'Общественным транспортом, ближайшая станция метро Охотный ряд. Выход №1 к ТЦ «Охотный ряд», из стеклянных дверей направо. По Моховой улице пройти 200м, далее у шлагбаума свернуть направо.Внимание! Для автобусов, микроавтобусов, личного транспорта парковка возле музея не предусмотрена.Вы можете воспользоваться парковками "Московского паркинга", расположенных по-близости от музея.Для посетителей с ограниченными возможностями здоровья парковка возможна по предварительному согласованию, тел. +7 (495) 692-09-43',
-            img: ''
-        },
 
 
-        {
-            id: 1,
-            title: 'Схема расположения музея',
-            content: '',
-            img: map
-        },
+    const [contactsArr, setContactsArr] = useState<any[]>([])
+    
+    useEffect(() => {
+        (async () => {
+            const res = await getContacts()
+            if (res) {
 
-        
-    ]
+                const objRes = Object.entries(res[0])
+                const newData = objRes.filter((item) => item[0] !== 'id').map((item, index) => {
+                    const value = ['Адрес', 'Проведение мероприятий', 'Лаборатория', 'СМИ', 'Как добраться', 'Схема расположения музея']
+
+                        if (item[0] === 'way') {
+                            return {
+                                id: index+1,
+                                title: value[index] || '',
+                                content: item[1],
+                                img: item[1]
+                            }
+                        } else {
+
+                            return {
+                                id: index+1,
+                                title: value[index] || '',
+                                content: item[1],
+                                img: ''
+                            
+                                }
+
+                        }
+
+      
+                        
+
+                    
+
+                })
+
+
+                setContactsArr(newData)
+            }
+        })()
+    }, [])
+
+
+    console.log(contactsArr)
 
 
   return (
@@ -96,7 +102,7 @@ const page: FC = () => {
                 
                 {
 
-                    (contactsArr.length < 1 || !contactsArr) ? <div>Нет данных</div> : contactsArr.map((item: contactsArrType, index: number): ReactNode => {
+                    (!contactsArr || contactsArr.length < 1) ? <div>Нет данных</div> : contactsArr.map((item: contactsArrType, index: number): ReactNode => {
                         return (
                             <Col key={index+1} className='mb-3'><MyAcardeon key={index+1} title={item.title} content={item.content} image={item.img}/></Col>
                         )
