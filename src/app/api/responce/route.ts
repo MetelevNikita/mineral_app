55bb9ccf85f246ab8992a9aa98ae519405e23943
Nikita Metelev <108466638+MetelevNikita@users.noreply.github.com>
2026-02-10 12:43:12 +0500

    fix security bags and delete all modals 1.1

--- src/app/api/responce/route.ts
+++ src/app/api/responce/route.ts
@@ -1,64 +1,64 @@
-// import { NextResponse } from "next/server";
-// import { PrismaClient } from "@/../generated/prisma/client.js";
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@/../generated/prisma/client.js";
 
-// // 
+// 
 
-// import { sendRandomCode } from "@/functions/emailRandomCode";
-// import { cookies } from "next/headers";
-
-
-// // 
-
-// const prisma = new PrismaClient()
-
-// // 
-
-// export const POST = async (req: Request) => {
-//   try {
+import { sendRandomCode } from "@/functions/emailRandomCode";
+import { cookies } from "next/headers";
 
 
-//     const generateRandomCode = () => {
-//       return Math.floor(100000 + Math.random() * 900000); // Генерация 6-значного числа
-//     }
+// 
+
+const prisma = new PrismaClient()
+
+// 
+
+export const POST = async (req: Request) => {
+  try {
 
 
-//     const { name, email, password, isAdmin } = await req.json()
+    const generateRandomCode = () => {
+      return Math.floor(100000 + Math.random() * 900000); // Генерация 6-значного числа
+    }
+
+
+    const { name, email, password, isAdmin } = await req.json()
     
-//     const findEmail = await prisma.user.findUnique({
-//         where: {
-//             email
-//         }
-//     })
+    const findEmail = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
 
-//     if (findEmail) {
-//         if (findEmail.email === email) {
-//             return NextResponse.json({message: "Email already exists"})
-//         }
-//     }
-
-
-//     // генерация кода
+    if (findEmail) {
+        if (findEmail.email === email) {
+            return NextResponse.json({message: "Email already exists"})
+        }
+    }
 
 
-//     const code = generateRandomCode();
-
-//     (await cookies()).set('code', code.toString());
-//     (await cookies()).set('email', email.toString());
+    // генерация кода
 
 
-//     console.log('Код после регистрации отправляется на почту:', code);
-//     await sendRandomCode(email, code);
+    const code = generateRandomCode();
+
+    (await cookies()).set('code', code.toString());
+    (await cookies()).set('email', email.toString());
+
+
+    console.log('Код после регистрации отправляется на почту:', code);
+    await sendRandomCode(email, code);
     
-//   } catch (error: Error | unknown) {
-//     if (error instanceof Error) {
-//       NextResponse.json({
-//         error: error.message
-//       })
-//     }
+  } catch (error: Error | unknown) {
+    if (error instanceof Error) {
+      NextResponse.json({
+        error: error.message
+      })
+    }
 
-//     NextResponse.json({
-//       error: error
-//     })
+    NextResponse.json({
+      error: error
+    })
     
-//   }
-// }
+  }
+}
