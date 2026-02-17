04cb4c0c31b269975a868df5456625f5ea55bc04
Nikita Metelev <108466638+MetelevNikita@users.noreply.github.com>
2026-02-17 17:40:50 +0500

    fix footer menu and new top menu

--- src/components/ui/MenuElement/Menuelement.tsx
+++ src/components/ui/MenuElement/Menuelement.tsx
@@ -1,6 +1,12 @@
-import { FC } from 'react'
+import { FC, useContext } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { ReactSVG } from 'react-svg';
+
+// context
+
+import { useMenuContext } from '@/app/main/layout'
+import { useTopMenuContext } from '@/app/main/layout'
 
 // style
 
@@ -11,17 +17,40 @@ import styles from './Menuelement.module.css'
 interface MenuelementProps {
     title: string,
     className: string,
-    icon: string,
+    icon: string | any,
     path: string,
     onClick?: () => void
 }
 
 const Menuelement: FC<MenuelementProps> = ({ title, icon, path, onClick, className }) => {
+
+
+  const {menuActive, setMenuActive} = useMenuContext()
+  const {topMenuOn, setTopMenuOn} = useTopMenuContext()
+  const isActive = menuActive === path
+
   return (
 
-    <Link className={className} style={{textDecoration: 'none'}} href={path}>
-        <Image src={icon} width={25} height={25} alt='menu_icon'/>
-        <div className={styles.menu_text}>{title}</div>
+    <Link onClick={() => {
+      setMenuActive(path)
+      setTopMenuOn(false)
+      }}
+      className={className}
+      style={{textDecoration: 'none'}}
+      href={path}>
+
+
+        <ReactSVG src={icon.src} beforeInjection={(svg) => {
+            // Меняем цвет всех элементов
+            svg.querySelectorAll('path').forEach(path => {
+              path.setAttribute('stroke', isActive ? '#FFBC41' : 'white');
+            });
+
+            svg.setAttribute('width', '25');
+            svg.setAttribute('height', '25');
+          }} />
+
+        <div className={styles.menu_text} style={{color: isActive ? '#FFBC41' : 'white'}}>{title}</div>
     </Link>
 
   )
